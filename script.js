3fd112e6eeb3c8cc70bb66532e6d306dc774150b
Vaughn1262 <vaughnpalmer62@gmail.com>
2024-02-10 11:16:12 -0700

    added comments

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const Key = '72194d221ce96885dbc793879b181b2b'; 
-const Url = 'https://api.openweathermap.org/data/2.5/weather';
-const inputLocation = document.getElementById('inputLocation');
+const Key = '72194d221ce96885dbc793879b181b2b'; //This is an API key that I got from openweathermap.com 
+const Url = 'https://api.openweathermap.org/data/2.5/weather'; // URL for the program to get the data from
+
+//This section sets up all needed variables for updating and checking in the logic
+const inputLocation = document.getElementById('inputLocation'); 
 const search = document.getElementById('searchButton');
 const locationElement = document.getElementById('location');
 const temperature = document.getElementById('temperature');
@@ -11,7 +13,8 @@ const airPressure = document.getElementById('airPressure');
 const weatherImage = document.querySelector(".weatherImage");
 const visibility = document.getElementById('visibility');
 
-
+//This code puts a check that occurs if the user clicks search button. 
+//If they have a location it will get the weather for that area.
 search.addEventListener('click', () => {
     const userLocation = inputLocation.value;
     if (userLocation){
@@ -19,6 +22,8 @@ search.addEventListener('click', () => {
     }
 });
 
+//This code checks if the user hits the enter key.
+//If they have a location in the search box it will get the weather for that area.
 inputLocation.addEventListener("keypress", function(event){
     if (event.key === "Enter"){
         const userLocation = inputLocation.value;
@@ -28,6 +33,9 @@ inputLocation.addEventListener("keypress", function(event){
     }
 });
 
+//This is the main logic for the program. When it is called it gets the weather for the location entered by the user from the internet
+//It gets many different things such as temperature, feels like temperature, updates the weather image, location text, description, humidity, airPressure, visibility.
+//It then sends the updated version to the webpage.
 function getWeather(userLocation){
     const url = `${Url}?q=${userLocation}&appid=${Key}&units=imperial`;
     fetch(url)
